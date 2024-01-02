8e88d2fc1a2fc7d226949fa4da8a0368c7ef7475
Benny Chrispin <ndizibaidu23@gmail.com>
2024-01-02 17:34:30 +0200

    first commit

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
+import express from "express";
+
 // create a server
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+// listen to some port
+app.listen(800, () => {
+  console.log("Server is running on http://localhost:800");
+});
